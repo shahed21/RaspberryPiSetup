8cc174d075d1429b352bc6552aa95423da2aeec9
shahed21 <shahed21@gmail.com>
2021-09-28 03:54:19 -0500

    need to return back to the original directory

--- geth_installer.sh
+++ geth_installer.sh
@@ -2,3 +2,4 @@ wget https://gethstore.blob.core.windows.net/builds/geth-linux-arm7-1.10.8-26675
 tar -xvf geth-linux-arm7-1.10.8-26675454.tar.gz
 cd geth-linux-arm7-1.10.8-26675454
 sudo mv geth /usr/local/bin/
+cd ..
