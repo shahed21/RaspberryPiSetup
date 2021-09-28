4080860c977ad000f11d7d4f9aecdfeb52b28bfc
shahed21 <shahed21@gmail.com>
2021-09-28 03:56:14 -0500

    move ssh config down before reboot

--- setup.sh
+++ setup.sh
@@ -5,16 +5,16 @@ sudo apt-get remove xrdp vnc4server tightvncserver
 sudo apt-get install tightvncserver
 sudo apt-get install xrdp
 
-sudo systemctl enable ssh
-sudo systemctl start ssh
-mkdir ~/.ssh
-cp ./.ssh/config ~/.ssh
-
 cat append.profile.txt >> ~/.profile
 source ~/.profile
 
 source go_installer.sh
 source geth_installer.sh
 source nodejs_installer.sh
+
+sudo systemctl enable ssh
+sudo systemctl start ssh
+mkdir ~/.ssh
+cp ./.ssh/config ~/.ssh
 
 sudo reboot
