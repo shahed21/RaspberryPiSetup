1270ba097e6da7e7b7098e3cb920aca6a3e95e6a
Shahed Rahim <shahed21@gmail.com>
2021-09-28 00:49:57 -0500

    setup.sh created

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # RaspberryPiSetup
 This repo is to automate Raspberry Pi Software Environment for Block Chain Programming
+
+After cloning this, just source the setup.sh script to setup Raspberry PI
---
+++ setup.sh
@@ -0,0 +1,10 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install build-essential
+sudo apt-get remove xrdp vnc4server tightvncserver
+sudo apt-get install tightvncserver
+sudo apt-get install xrdp
+
+sudo systemctl enable ssh
+sudo systemctl start ssh
+
