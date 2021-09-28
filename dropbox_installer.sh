f20ca89ed9bf36293f7217de9742d13a44a26283
shahed21 <shahed21@gmail.com>
2021-09-28 11:49:11 -0500

    Create dropbox_installer.sh

---
+++ dropbox_installer.sh
@@ -0,0 +1,8 @@
+cd ~
+git clone https://github.com/andreafabrizi/Dropbox-Uploader.git
+cd ~/Dropbox-Uploader/
+sudo chmod +x dropbox_uploader.sh
+./dropbox_uploader.sh
+cd ~
+~/Dropbox-Uploader/dropbox_uploader.sh download /raspi/keys/.ssh
+cd ~/RaspberryPiSetup
