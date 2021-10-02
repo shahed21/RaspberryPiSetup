b13f834b64584cd358563010d3eac7a28dbfeb4d
Shahed Rahim <shahed21@gmail.com>
2021-10-01 22:03:42 -0500

    added truffle installation

--- nodejs_installer.sh
+++ nodejs_installer.sh
@@ -3,3 +3,5 @@ sudo rm /usr/local/lib/nodejs
 sudo mkdir /usr/local/lib/nodejs
 sudo tar -xJvf node-v14.17.6-linux-armv7l.tar.xz -C /usr/local/lib/nodejs
 rm node-v14.17.6-linux-armv7l.tar.xz
+sudo chown -R $USER /usr/local/lib/nodejs/node-v14.17.6-linux-armv7l/
+npm install -g truffle
